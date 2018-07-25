7865bef0d6ab637ab1bba6c7fdd02948f52821fe
Steve Edwards (UST) <sedwards@microsoft.com>
2018-07-25 16:19:18 -0500

    Powershell Script to create new Key Vault and New Cert for use with SF

---
+++ Security/CreateKeyVaultAndCertificateForServiceFabric.ps1
@@ -0,0 +1,39 @@
+Param(
+    [string] [Parameter(Mandatory=$true)] $SubscriptionId,
+    [string] [Parameter(Mandatory=$true)] $Location,
+    [string] [Parameter(Mandatory=$true)] $ResourceGroup,
+    [string] [Parameter(Mandatory=$true)] $VaultName,
+    [string] [Parameter(Mandatory=$true)] $CommonName
+)
+
+Set-StrictMode -Version 3
+
+function Check-Session () {
+    $Error.Clear()
+
+    #if context already exist
+    Get-AzureRmContext -ErrorAction Continue
+    foreach ($eacherror in $Error) {
+        if ($eacherror.Exception.ToString() -like "*Run Login-AzureRmAccount to login.*") {
+            Login-AzureRmAccount
+        }
+    }
+
+    $Error.Clear();
+}
+
+$ErrorActionPreference = "Stop"
+
+Check-Session
+Set-AzureRmContext -SubscriptionId $subscriptionId -ErrorAction Stop
+
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $location -Force
+
+if(!(Get-AzureRmResource -ResourceName $VaultName -ResourceGroupName $ResourceGroup)) {
+  New-AzureRmKeyVault -VaultName $VaultName -ResourceGroupName $ResourceGroup  -Location $Location -EnabledForDeployment
+}
+
+$policy = New-AzureKeyVaultCertificatePolicy -SubjectName $CommonName -IssuerName Self -ValidityInMonths 12
+Add-AzureKeyVaultCertificate -VaultName $VaultName -Name $CertificateName -CertificatePolicy $policy
+
+Write-Host "operation complete"
