38749ec34b19e6ab7387671e0e3aa71db490c864
Steve Edwards <sedwardstx@users.noreply.github.com>
2021-01-13 14:05:37 -0600

    Update DisableIPProvider.ps1

--- Scripts/DisableIPProvider.ps1
+++ Scripts/DisableIPProvider.ps1
@@ -2,7 +2,7 @@
 # ------------------------------------------------------------
 # Copyright (c) Microsoft Corporation.  All rights reserved.
 # Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-# Feedback : pkc@microsoft.com
+# Feedback : sedwards@microsoft.com
 # ------------------------------------------------------------
 
 <#
